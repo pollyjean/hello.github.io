ebe64a0ea59a16995b46526f829eaf4b0d69e734
pollyjean <pollyjean@naver.com>
2023-05-01 00:06:25 +0900

    스크립트 순서 오류 수정, 날씨 이미지 수정

--- js/greetings.js
+++ js/greetings.js
@@ -8,6 +8,34 @@ const loginInput = loginForm.querySelector("input");
 const HIDDEN_CLASS_NAME = "hidden";
 const USERNAME_KEY = "username";
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+const paintGreeting = () => {
+  const dayNight = searchDayNight();
+  const hour = parseInt(new Date().getHours());
+  let greetingTxt = "";
+  if (dayNight === "d") {
+    greetingTxt = hour > 11 ? "Good afternoon" : "Good morning";
+  } else if (dayNight === "n") {
+    greetingTxt = hour > 23 ? "Good night" : "Good evening";
+  }
+  return greetingTxt;
+};
+const switchToGreeting = (username) => {
+  const greetingTxt = paintGreeting();
+  document.title = `${greetingTxt} ${username}. | Hello App`;
+  greeting.innerText = `${greetingTxt}, ${username}.`;
+  greeting.classList.remove(HIDDEN_CLASS_NAME);
+  greeting.addEventListener("dblclick", handleRemoveName);
+};
+const handleRemoveName = () => {
+  if (window.confirm("Reset this name?")) {
+    document.title = `paintGreeting(). | Hello App`;
+    localStorage.removeItem(USERNAME_KEY);
+    greeting.classList.add(HIDDEN_CLASS_NAME);
+    loginForm.classList.remove(HIDDEN_CLASS_NAME);
+    loginInput.value = "";
+    loginInput.focus();
+  }
+};
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASS_NAME);
@@ -21,34 +49,3 @@ if (savedUsername === null) {
 } else {
   switchToGreeting(savedUsername);
 }
-
-const paintGreeting = () => {
-  const dayNight = searchDayNight();
-  const hour = parseInt(new Date().getHours());
-  let greetingTxt = "";
-  if (dayNight === "d") {
-    greetingTxt = hour > 11 ? "Good afternoon" : "Good morning";
-  } else if (dayNight === "n") {
-    greetingTxt = hour > 23 ? "Good night" : "Good evening";
-  }
-  return greetingTxt;
-};
-
-const switchToGreeting = (username) => {
-  const greetingTxt = paintGreeting();
-  document.title = `${greetingTxt} ${username}. | Hello App`;
-  greeting.innerText = `${greetingTxt}, ${username}.`;
-  greeting.classList.remove(HIDDEN_CLASS_NAME);
-  greeting.addEventListener("dblclick", handleRemoveName);
-};
-
-const handleRemoveName = () => {
-  if (window.confirm("Reset this name?")) {
-    document.title = `paintGreeting(). | Hello App`;
-    localStorage.removeItem(USERNAME_KEY);
-    greeting.classList.add(HIDDEN_CLASS_NAME);
-    loginForm.classList.remove(HIDDEN_CLASS_NAME);
-    loginInput.value = "";
-    loginInput.focus();
-  }
-};
